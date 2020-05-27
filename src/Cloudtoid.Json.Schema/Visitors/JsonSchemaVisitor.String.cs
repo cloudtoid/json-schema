7d59a8cb2e7e4c9ed9ee569677d78dbca7da5efe
Pedram Rezaei <pedramr@microsoft.com>
2020-05-27 13:11:26 -0700

    tint refactoring

---
+++ src/Cloudtoid.Json.Schema/Visitors/JsonSchemaVisitor.String.cs
@@ -0,0 +1,24 @@
+﻿namespace Cloudtoid.Json.Schema
+{
+    public abstract partial class JsonSchemaVisitor
+    {
+        protected internal virtual void VisitString(JsonSchemaString constraint)
+        {
+            // visit self
+            VisitConstraint(constraint);
+
+            // visit children
+            if (constraint.ContentMediaType != null)
+                VisitStringContentMedia(constraint.ContentMediaType, constraint.ContentSchema);
+        }
+
+        protected virtual void VisitStringContentMedia(string contentMediaType, JsonSchemaSubSchema? contentSchema)
+        {
+            if (contentSchema != null)
+                VisitStringContentSchema(contentSchema);
+        }
+
+        protected virtual void VisitStringContentSchema(JsonSchemaSubSchema element)
+            => Visit(element);
+    }
+}
--- src/Cloudtoid.Json.Schema/Visitors/JsonSchemaVisitor.cs
+++ src/Cloudtoid.Json.Schema/Visitors/JsonSchemaVisitor.cs
@@ -24,24 +24,5 @@
 
         protected internal virtual void VisitInteger(JsonSchemaInteger constraint)
             => VisitConstraint(constraint);
-
-        protected internal virtual void VisitString(JsonSchemaString constraint)
-        {
-            // visit self
-            VisitConstraint(constraint);
-
-            // visit children
-            if (constraint.ContentMediaType != null)
-                VisitStringContentMedia(constraint.ContentMediaType, constraint.ContentSchema);
-        }
-
-        protected virtual void VisitStringContentMedia(string contentMediaType, JsonSchemaSubSchema? contentSchema)
-        {
-            if (contentSchema != null)
-                VisitStringContentSchema(contentSchema);
-        }
-
-        protected virtual void VisitStringContentSchema(JsonSchemaSubSchema element)
-            => Visit(element);
     }
 }
